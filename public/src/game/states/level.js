67d0abfac2db1e186c9e27713b71c9aa3f1eff89
Andrew Hung <druphoria@utexas.edu>
2015-01-15 20:58:52 -0800

    Randomized spawn and moved more stuff into player class

--- public/src/game/states/level.js
+++ public/src/game/states/level.js
@@ -13,39 +13,10 @@ Level.prototype = {
   create: function () {
   	socket = io("http://localhost:8120"); // By default, this connects to the local host.
     
-    player = new Player(game.camera.width / 2, game.camera.height / 2);
-
-    this.initializeAnimations();
-  },
-
-  initializeAnimations: function() {
-  	player.animations.add('down', [0, 1, 2, 3, 4], 10, true);
-  	player.animations.add('up', [5, 6, 7, 8, 9], 10, true);
-  	player.animations.add('right', [10, 11, 12], 10, true);
-  	player.animations.add('left', [13, 14, 15], 10, true);
+    player = new Player(Math.round(Math.random() * game.camera.width), Math.round(Math.random() * game.camera.height));
   },
 
   update: function() {
-  	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-  		player.body.velocity.y = 0;
-  		player.body.velocity.x = -200;
-  		player.animations.play('left');
-  	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-  		player.body.velocity.y = 0;
-  		player.body.velocity.x = 200;
-  		player.animations.play('right');
-  	} else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-  		player.body.velocity.x = 0;
-  		player.body.velocity.y = -200;
-  		player.animations.play('up');
-  	} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-  		player.body.velocity.x = 0;
-  		player.body.velocity.y = 200;
-  		player.animations.play('down');
-  	} else {
-  		player.body.velocity.x = 0;
-  		player.body.velocity.y = 0;
-  		player.animations.stop();
-  	}
+  	player.move();
   }
 };
