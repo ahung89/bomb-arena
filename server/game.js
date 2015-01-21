4f20fc45cf86cf7d65a4787dc62d52e1abdd183e
Andrew Hung <druphoria@utexas.edu>
2015-01-20 23:22:49 -0800

    changed back to not use http

--- server/game.js
+++ server/game.js
@@ -1,7 +1,6 @@
 // Dependencies
 var util = require('util');
-var app = require('http').createServer();
-var io = require('socket.io')(app);
+var io = require('socket.io');
 
 // Game objects
 var Player = require('./entities/player');
@@ -16,8 +15,8 @@ var bombs = {};
 init();
 
 function init() {
-	// socket = io.listen(process.env.PORT || 8120);
-	app.listen(process.env.PORT || 8120);
+	socket = io.listen(process.env.PORT || 8120);
+	// app.listen(process.env.PORT || 8120);
 
 	// Begin listening for events.
 	setEventHandlers();
@@ -27,7 +26,7 @@ function init() {
 };
 
 function setEventHandlers () {
-	io.on("connection", function(client) {
+	socket.sockets.on("connection", function(client) {
 		util.log("New player has connected: " + client.id);
 
 		client.on("new player", onNewPlayer);
@@ -83,7 +82,7 @@ function onPlaceBomb(data) {
 	setTimeout(function() {
 		delete bombs[playerId][bombId];		
 		util.log("deleting bomb " + bombId);
-		io.sockets.emit("detonate", {id: bombId});
+		socket.sockets.emit("detonate", {id: bombId});
 	}, 3000);
 
 	this.broadcast.emit("place bomb", {x: data.x, y: data.y, id: data.id});
