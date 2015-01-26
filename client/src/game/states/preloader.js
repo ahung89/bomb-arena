9ba5ee5065ab7c23c4a5fd155c06ce97e08327b3
Andrew Hung <druphoria@utexas.edu>
2015-01-25 21:14:47 -0800

    fix naming

--- client/src/game/states/preloader.js
+++ client/src/game/states/preloader.js
@@ -8,7 +8,7 @@ Preloader.prototype = {
     this.load.spritesheet("bomberman", "assets/sprites/bomberman.png", 28, 50);
     this.load.spritesheet("bomb", "assets/sprites/bomb.png", 26, 40);
 
-    this.load.tilemap("levelOne", 'assets/levels/levelOne.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.tilemap("levelOne", 'assets/levels/level_one.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image("tiles", "assets/tiles/tileset.png");
   },
 
