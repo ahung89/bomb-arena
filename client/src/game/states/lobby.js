1769a0676705ddea6f3d939c4afd39dbc5f138ac
Andrew Hung <druphoria@utexas.edu>
2015-02-17 22:15:49 -0800

    added buttons

--- client/src/game/states/lobby.js
+++ client/src/game/states/lobby.js
@@ -5,23 +5,35 @@ var slotXOffset = 25;
 var lobbySlotDistance = 110;
 var numLobbySlots = 4;
 
+var buttonYOffsetWithinSlot = 34;
+var buttonXOffsetWithinSlot = 400;
+
 module.exports = Lobby;
 
 Lobby.prototype = {
 	create: function() {
 		this.slots = [];
 		this.buttons = [];
-		
-		this.drawSlots();
+
+		this.addSlots();
+		this.addButtons();
 	},
 
 	update: function() {
 
 	},
 
-	drawSlots: function() {
+	addSlots: function() {
 		for(var i = 0; i < numLobbySlots; i++) {
 			this.slots[i] = game.add.sprite(slotXOffset, initialSlotYOffset + i * lobbySlotDistance, "lobby_slot");
+		}
+	},
+
+	addButtons: function() {
+		for(var i = 0; i < numLobbySlots; i++) {
+			this.buttons[i] = game.add.button(slotXOffset + buttonXOffsetWithinSlot, 
+				initialSlotYOffset + i * lobbySlotDistance + buttonYOffsetWithinSlot,
+				 "join_button", null, null, 1, 0);
 		}
 	}
 };
