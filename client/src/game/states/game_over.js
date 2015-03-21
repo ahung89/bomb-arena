f55f92e46fca0ef1ee5916e80e3eea5a214d4e51
Andrew Hung <druphoria@utexas.edu>
2015-03-20 23:23:08 -0700

    implemented rudimentary game over screen

--- client/src/game/states/game_over.js
+++ client/src/game/states/game_over.js
@@ -1,4 +1,18 @@
+var TextConfigurer = require('../util/text_configurer');
+
 function GameOver() {
+}
+
+GameOver.prototype = {
+	init: function(winner) {
+		this.winner = winner;
+	},
+
+	create: function() {
+		var text = game.add.text(game.camera.width / x, game.camera.height / 2, "Game Over. Winner is " + this.winner);
+		text.anchor.setTo(0.5, 0.5);
+		TextConfigurer.configureText(text, "white", 28);
+	}
 }
 
 module.exports = GameOver;
